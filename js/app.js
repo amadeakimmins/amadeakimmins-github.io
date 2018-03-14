a00b123fa9a7e2aecdf1fc428597c532ad59abde
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2018-03-14 10:52:37 +0000

    added time buttons all working and links open in new tab

--- js/app.js
+++ js/app.js
@@ -4,21 +4,24 @@ $(document).ready(function() {
     scrollingSpeed: 600
   }
   );
-});
 
-// function init() {
-//
-//   $(document).ready(function(){
-//
-//     $('#fullpage').fullpage({
-//       anchors: ['about', 'projects', 'skills', 'contact']
-//     });
-//
-//
-//
-//   });
-//
-// }
-//
-//
-// init();
+  // HIDING DIVS DEPENDING ON TIME CHOSEN
+  const $30SecsButton = $('.30SecsButton');
+  const $5MinsButton = $('.5MinsButton');
+  const $page2 = $('.page2');
+  const $page4 = $('.page4');
+
+  function smallPortfolio() {
+
+    $page2.addClass('hidden');
+    $page4.addClass('hidden');
+  }
+
+  function longPortfolio() {
+    $page2.removeClass('hidden');
+    $page4.removeClass('hidden');
+  }
+
+  $30SecsButton.on('click', smallPortfolio);
+  $5MinsButton.on('click', longPortfolio);
+});
